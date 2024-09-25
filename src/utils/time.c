1980cdc9ee00d3de2c616e7f3a072ebecd4abd04
prixladi <elix78963@gmail.com>
2024-09-25 22:16:30 +0200

    refactor(daemon): Renamed utils/threading to utils/time

--- src/utils/time.c
+++ src/utils/time.c
@@ -1,9 +1,20 @@
 #include <pthread.h>
 #include <time.h>
 
-#include "threading.h"
+#include "time.h"
 
 #define ms_to_ns(x) x * 1000 * 1000
+#define us_to_ns(x) x * 1000
+
+void sleep_ms(int ms)
+{
+    sleep_ns(ms_to_ns(ms));
+}
+
+void sleep_us(int us)
+{
+    sleep_ns(us_to_ns(us));
+}
 
 void sleep_ns(int ns)
 {
@@ -13,9 +24,4 @@ void sleep_ns(int ns)
     struct timespec r;
 
     nanosleep(&t, &r);
-}
-
-void sleep_ms(int ms)
-{
-    sleep_ns(ms_to_ns(ms));
 }
