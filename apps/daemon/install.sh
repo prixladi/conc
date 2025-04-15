778507427127aff33d312d74f72e9488654202b4
prixladi <elix78963@gmail.com>
2025-04-15 12:22:28 +0200

    feat(daemon): Change target for daemon install section

--- apps/daemon/install.sh
+++ apps/daemon/install.sh
@@ -31,7 +31,7 @@ Restart=always
 RestartSec=3
 
 [Install]
-WantedBy=multi-user.target"
+WantedBy=default.target"
 SERVICE_CONFIG_LOCATION=$SYSTEMD_PREFIX/concd.service
 
 mkdir -p "$SYSTEMD_PREFIX"
